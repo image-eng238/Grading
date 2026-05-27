326e621d8ba5a8e4dc16f3a4101b6518868a0897
taketo <26m206@st.takushoku-u.ac.jp>
2026-05-27 20:01:28 +0900

    add execution script

---
+++ execute.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+bin_dir='.'
+out_dir='.'
+work_pane='1'
+work_pipe=''
+work_args=''
+
+while getopts a:b:o:t:p: opt; do
+	case $opt in
+	a)
+	work_args=$OPTARG
+	;;
+	b)
+	bin_dir=$OPTARG
+	;;
+	o)
+	out_dir=$OPTARG
+	;;
+	t)
+	work_pane=$OPTARG
+	;;
+	p)
+	work_pipe=$OPTARG
+	;;
+	*)
+	exit 1
+	;;
+	esac
+done
+
+bin_files=($(ls $bin_dir))
+
+for bin in "${bin_files[@]}"; do
+	echo $bin is runnig...
+	echo $bin > $out_dir/$bin.log
+	tmux send-key -t $work_pane \
+	-- "$bin_dir/$bin $work_args >> $out_dir/$bin.log" \
+	Enter
+	sleep 0.1
+
+	tmux list-pane -sF "#{pane_id} #{pane_current_command}" \
+		| grep -qE "%$work_pane .*$bin\$"
+	if [ $? -eq 0 ]; then
+		tmux send-key -t $work_pane \
+		-- "$work_pipe" \
+		Enter
+	fi
+	sleep 0.1
+
+	tmux list-pane -sF "#{pane_id} #{pane_current_command}" \
+		| grep -qE "%$work_pane .*$bin\$"
+	if [ $? -eq 0 ]; then
+        	tmux send-key -t $work_pane -- ^C
+		echo "> failure" | tee -a $out_dir/$bin.log
+		sleep 0.1
+	else
+		echo "> success" | tee -a $out_dir/$bin.log
+	fi
+done
+
